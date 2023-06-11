b5f5df8acd680a888a40ef98526a720f5805f131
Amitosh <amitosh.verma9@gmail.com>
2023-06-12 00:16:43 +0530

    added bootstrap 5 css and js
    Signed-off-by: Amitosh <amitosh.verma9@gmail.com>

--- src/main/java/com/example/AnotherManagementApp/Controller/userController.java
+++ src/main/java/com/example/AnotherManagementApp/Controller/userController.java
@@ -1,2 +1,24 @@
-package com.example.AnotherManagementApp.Controller;public class userController {
+package com.example.AnotherManagementApp.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class userController {
+
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+
+    @GetMapping("/register")
+    public String register(){
+        return "register";
+    }
+
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+
 }
