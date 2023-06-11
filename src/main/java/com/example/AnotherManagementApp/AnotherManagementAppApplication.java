1b3a978168ff60cc639209701796c464a1ce2509
Amitosh <amitosh.verma9@gmail.com>
2023-06-11 16:35:58 +0530

    Started with maven using Spring initializer
    Signed-off-by: Amitosh <amitosh.verma9@gmail.com>

---
+++ src/main/java/com/example/AnotherManagementApp/AnotherManagementAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.AnotherManagementApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnotherManagementAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnotherManagementAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/AnotherManagementApp/AnotherManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AnotherManagementApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnotherManagementAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
