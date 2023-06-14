647dd10d4fb00ecc1787139175ebd9dac3ba11f4
Amitosh <amitosh.verma9@gmail.com>
2023-06-14 16:12:41 +0530

    Enabling web security with roles
    Signed-off-by: Amitosh <amitosh.verma9@gmail.com>

--- src/main/java/com/example/AnotherManagementApp/Config/SecurityConfig.java
+++ src/main/java/com/example/AnotherManagementApp/Config/SecurityConfig.java
@@ -1,2 +1,21 @@
-package com.example.AnotherManagementApp.Config;public class SecurityConfig {
+package com.example.AnotherManagementApp.Config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+
+        //provided role
+        http.authorizeRequests().antMatchers("/admin/**").hasRole("ADMIN")
+                .antMatchers("/user/**").hasRole("USER")
+                .antMatchers("/**").permitAll().and().formLogin().loginPage("/signin").loginProcessingUrl("/login")
+                .defaultSuccessUrl("/user/").and().csrf().disable()
+    }
 }
