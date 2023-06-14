adfe4b2cd1706cebd335ad0943eee191b391cbb8
Amitosh <amitosh.verma9@gmail.com>
2023-06-14 13:15:50 +0530

    renamed userController to homeController
    Signed-off-by: Amitosh <amitosh.verma9@gmail.com>

--- src/main/java/com/example/AnotherManagementApp/Controller/homeController.java
+++ src/main/java/com/example/AnotherManagementApp/Controller/homeController.java
@@ -5,13 +5,12 @@ import com.example.AnotherManagementApp.Service.UserServiceImpl;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
-public class userController {
+public class homeController {
 
     @Autowired
     private UserServiceImpl service;
@@ -27,7 +26,7 @@ public class userController {
         return "register";
     }
 
-    @GetMapping("/login")
+    @GetMapping("/signin")
     public String login(){
         return "login";
     }
