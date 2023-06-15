44bd360dfd0d38e530fde2272b101b7f22c03011
Amitosh <amitosh.verma9@gmail.com>
2023-06-15 14:19:32 +0530

    Apis for user page all users will be here
    Signed-off-by: Amitosh <amitosh.verma9@gmail.com>

--- src/main/java/com/example/AnotherManagementApp/Controller/homeController.java
+++ src/main/java/com/example/AnotherManagementApp/Controller/homeController.java
@@ -2,7 +2,7 @@ package com.example.AnotherManagementApp.Controller;
 
 import com.example.AnotherManagementApp.Entity.User;
 import com.example.AnotherManagementApp.Service.UserServiceImpl;
-import jakarta.servlet.http.HttpSession;
+import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
