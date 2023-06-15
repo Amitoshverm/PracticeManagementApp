30ab2e1266661b5ba7f87b271a884313fa8efbde
Amitosh <amitosh.verma9@gmail.com>
2023-06-15 14:20:15 +0530

    Apis for user page all users will be here
    Signed-off-by: Amitosh <amitosh.verma9@gmail.com>

--- src/main/java/com/example/AnotherManagementApp/Controller/userController.java
+++ src/main/java/com/example/AnotherManagementApp/Controller/userController.java
@@ -5,8 +5,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/user/")
+@RequestMapping("/user")
 public class userController {
 
 
+    @GetMapping("/")
+    public String home(){
+        return "user/home";
+    }
 }
